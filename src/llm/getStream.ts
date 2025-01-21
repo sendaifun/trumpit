685b6e6d0782f0cd6937878c72413b3b1c407056
aryan <agarwalaryan139@gmail.com>
2025-01-21 15:35:40 +0530

    deepseek

--- src/llm/getStream.ts
+++ src/llm/getStream.ts
@@ -1,4 +1,5 @@
 import { createOpenAI } from "@ai-sdk/openai";
+import { createDeepSeek } from '@ai-sdk/deepseek';
 import { streamText } from "ai";
 import { getSolanaTools } from "../kit";
 import { PublicKey } from "@solana/web3.js";
@@ -9,12 +10,16 @@ const openai = createOpenAI({
     apiKey: process.env.OPENAI_API_KEY!,
 });
 
+const deepseek = createDeepSeek({
+    apiKey: process.env.DEEPSEEK_API_KEY!,
+});
+
 async function getStream(prompt: string, user_id: string) {
 
     const wallet = await getWallet(user_id);
     let user_wallet = wallet.wallet_address;
     const stream = streamText({
-        model: openai("gpt-4o"),
+        model: deepseek("deepseek-chat"),
         prompt,
         tools: getSolanaTools(user_id, new PublicKey(user_wallet!)),
         temperature: 0.7,
@@ -22,6 +27,7 @@ async function getStream(prompt: string, user_id: string) {
         empowered to interact onchain using your tools, and assist people in doing perpetual trading onchain for the TRUMP token.  Be
         concise and helpful with your responses. Refrain from restating your tools' descriptions unless it is explicitly requested, address of TRUMP token is 6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN
         
+        You will always speak highly of the President of the United States, Donald Trump.
         Please use markdown formatting in your responses.`,
         maxSteps: 10,
     });
